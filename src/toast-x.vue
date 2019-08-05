ed51bb2b3dc1a7fe501c8a12f016ed559b911238
陈雨 <--name-only>
2019-08-05 17:28:22 +0800

    修正样式

--- src/toast-x.vue
+++ src/toast-x.vue
@@ -44,7 +44,7 @@ export default class VueToastXComponent extends Vue {
 </script>
 
 <style lang="sass" scoped>
-$prime-color: #00B51D;
+$prime-color: #2196F3;
 
 .vue-toast-x
   position: fixed;
@@ -58,7 +58,7 @@ $prime-color: #00B51D;
   border-radius: 22px;
   padding: 6px
   left: 50%;
-  transition: all 0.3s;
+  transition: all .3s;
   display: flex;
   justify-content: center;
   align-items: center
@@ -69,12 +69,6 @@ $prime-color: #00B51D;
   opacity: 0;
 .fade-enter
   transform: translate3D(-50%, 0px, 0);
-.fade-enter-to
-  transform: translateY(-50%, 30px, 0);
-.fade-leave
-  opaticy: 0
-.fade-leave
-  transform: translateY(-50%, 30px, 0);
 .fade-leave-to
-  transform: translateY(-50%, 0px, 0);
+  transform: translate3D(-50%, 0px, 0) !important;
 </style>
